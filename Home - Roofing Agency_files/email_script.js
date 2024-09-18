7cd20db38946249e09181d9f89834ae546f1eccb
Swaraj Kumar Singh <sswaraj169@gmail.com>
2024-09-18 14:48:09 +0530

    added email validation

--- Home - Roofing Agency_files/email_script.js
+++ Home - Roofing Agency_files/email_script.js
@@ -1,31 +1,45 @@
+import { EMAILJS_ID, EMAILJS_TEMPLATE_ID } from "./keys.js";
+
 function sendMail() {
-  // Capture form data
   let name = document.getElementById("wpforms-186-field_0").value;
   let email = document.getElementById("wpforms-186-field_7").value;
   let phone = document.getElementById("wpforms-186-field_8").value;
   let description = document.getElementById("wpforms-186-field_5").value;
   let message = document.getElementById("wpforms-186-field_2").value;
 
-  // Prepare parameters
-  let params = {
-    name: name,
-    email: email,
-    phone: phone,
-    subject: "Email request from Pawzzz.in from " + name,
-    message: message,
-    description: description,
-    from_email: email,
-  };
+  if (
+    !isEmpty(name) ||
+    !isEmpty(email) ||
+    !isEmpty(phone) ||
+    !isEmpty(description) ||
+    !isEmpty(message)
+  ) {
+    alert("Please enter all the fields");
+    return;
+  } else {
+    let params = {
+      name: name,
+      email: email,
+      phone: phone,
+      subject: "Email request from Pawzzz.in from " + name,
+      message: message,
+      description: description,
+      from_email: email,
+    };
 
-  emailjs
-    .send("service_ni9gews", "template_pf433zu", params)
-    .then(() => {
-      alert("Email sent successfully!");
-      // Reload the page
-      console.log("Done successfully")
-      window.location.reload();
-    })
-    .catch((e) => {
-      console.log("Failed to send email", e);
-    });
+    emailjs
+      .send(EMAILJS_ID, EMAILJS_TEMPLATE_ID, params)
+      .then(() => {
+        alert("Email sent successfully!");
+        console.log("Done successfully");
+        window.location.reload();
+      })
+      .catch((e) => {
+        console.log("Failed to send email", e);
+      });
+  }
+}
+
+function isEmpty(str) {
+  return str == null || str.trim() === "";
 }
